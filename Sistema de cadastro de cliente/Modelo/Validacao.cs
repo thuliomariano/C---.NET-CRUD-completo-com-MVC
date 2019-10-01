0daed4a23706f8abd9d2ced3ee4342f002927254
Thulio <thulio.mariano@gmail.com>
2019-10-01 15:53:25 -0300

    Implementado metodo para a classe validação

--- Sistema de cadastro de cliente/Modelo/Validacao.cs
+++ Sistema de cadastro de cliente/Modelo/Validacao.cs
@@ -6,7 +6,34 @@ using System.Threading.Tasks;
 
 namespace Sistema_de_cadastro_de_cliente.Modelo
 {
-    class Validacao
+    
+    public class Validacao
     {
+        private String mensagem;
+
+        public string Mensagem { get => mensagem; set => mensagem = value; }
+
+        public Validacao(Cliente cliente)
+        {
+            mensagem = "";
+            if (cliente.Nome.Length > 50)
+            {
+                mensagem = "Quantidades de caracteres Excede o valor maximo(50)";
+            }
+            if (cliente.SobreNome.Length > 70)
+            {
+                mensagem = "Quantidades de caracteres Excede o valor maximo(70)";
+            }
+            if (cliente.Rg > 9)
+            {
+                mensagem = "Quantidades de valores númericos Excede o valor maximo(9)";
+            }
+            if (cliente.Cpf > 11)
+            {
+                mensagem = "Quantidades de valores númericos Excede o valor maximo(11)";
+            }
+                      
+
+        }
     }
 }
