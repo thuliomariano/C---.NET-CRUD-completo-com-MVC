b96889a0d341a31f2afb3d6b8cabcf2b21381666
Thulio <thulio.mariano@gmail.com>
2019-10-01 08:53:19 -0300

    Atribuindo atributos na classe Pessoa

--- Sistema de cadastro de cliente/Modelo/Cliente.cs
+++ Sistema de cadastro de cliente/Modelo/Cliente.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace Sistema_de_cadastro_de_cliente.Modelo
 {
-    class Cliente
+    public class Cliente
     {
+        private int id;
+        private String nome;
+        private String sobreNome;
+        private long rg;
+        private long cpf;
+        private DateTime dataDeNascimento;
+
+        public int Id { get => id;}   // não é possivel modificar o ID, somente ler
+        public string Nome { get => nome; set => nome = value; }
+        public string SobreNome { get => sobreNome; set => sobreNome = value; }
+        public long Rg { get => rg; set => rg = value; }
+        public long Cpf { get => cpf; set => cpf = value; }
+        public DateTime DataDeNascimento { get => dataDeNascimento; set => dataDeNascimento = value; }
     }
 }
