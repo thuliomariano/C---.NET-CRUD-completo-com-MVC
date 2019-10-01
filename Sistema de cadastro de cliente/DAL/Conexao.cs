43809e3e99a22d987aaf96c7c14048693df789b4
Thulio <thulio.mariano@gmail.com>
2019-10-01 08:36:28 -0300

    Atribuido valores a clase conexao

--- Sistema de cadastro de cliente/DAL/Conexao.cs
+++ Sistema de cadastro de cliente/DAL/Conexao.cs
@@ -3,10 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace Sistema_de_cadastro_de_cliente.DAL
 {
     public class Conexao
     {
+        SqlConnection connection;
+
+        public Conexao()
+        {
+            connection = new SqlConnection();
+
+            connection.ConnectionString = @"Data Source=widdian;Initial Catalog=Lasertech;Integrated Security=True";
+
+        }
+
+        public SqlConnection Conectar()
+        {
+            if (connection.State == System.Data.ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+
+            return connection;
+        }
+
+        public void Desconectar()
+        {
+            if (connection.State == System.Data.ConnectionState.Open)
+            {
+                connection.Close();
+            }
+        }
     }
 }
